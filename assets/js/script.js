c8efba57ab45add16d53f9d9d6a67eb276888639
Víttor Franceschi <89648256+Vacf04@users.noreply.github.com>
2023-08-29 11:53:09 -0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@ const searchInput = document.querySelector('.search-city');
 const searchSubmit = document.querySelector('.search-submit');
 const weatherDisplay = document.querySelector('.display-weather-data'); 
 const weatherIcon = document.querySelector('.weather-icon');
-const apiKey = "c121be5508016a4d90ae0911ffd16b03"
+const apiKey = "c121be5508016a4d90ae0911ffd16b03";
 
 searchSubmit.addEventListener('click', e => {
   e.preventDefault();
@@ -20,8 +20,9 @@ async function cityWeatherData() {
     displayWeatherData(weatherDatas[0], weatherDatas[1], name, country);
     searchInput.value = '';
   } catch(e) {
-    alert('Error 404 city not found!!')
-  }
+    alert('Error 404 city not found!!');
+    console.log(e);
+  };
 
 };
 
@@ -30,7 +31,7 @@ async function cityData() {
   const cityData = await fetch(getUrlCity(cityName));
   const cityDataJson = await cityData.json();
   return cityDataJson;
-}
+};
 
 async function weatherData(lat, lon) {
   const weatherData = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`);
@@ -39,7 +40,7 @@ async function weatherData(lat, lon) {
   const condition = weatherDataJson.weather[0].main;
   const weatherDatas = [temperature, condition]
   return weatherDatas;
-}
+};
 
 function displayWeatherData(temperature, condition, cityName, country) {
   let icon;
@@ -57,15 +58,15 @@ function displayWeatherData(temperature, condition, cityName, country) {
   }
   else if(condition === 'Mist') {
     icon = "assets/img/mist.svg";
-  }
+  };
   weatherDisplay.innerHTML = `
   <img src="${icon}" alt="weather-icon-status" class="weather-icon">
   <h2>${temperature}°c</h2>
   <h3>${cityName}, ${country}</h3>
   `;
-}
+};
 
  function getUrlCity(city) {
   const urlCity = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${apiKey}`;
   return urlCity;
- }
+ };
