863cfe163f4c0993aff1357a4c5b1bffdf1df698
Víttor <vittorfranceschi04@gmail.com>
2023-08-30 09:57:52 -0300

    fix-commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,14 +2,18 @@ const searchInput = document.querySelector('.search-city');
 const searchSubmit = document.querySelector('.search-submit');
 const weatherDisplay = document.querySelector('.display-weather-data'); 
 const weatherIcon = document.querySelector('.weather-icon');
-const apiKey = "c121be5508016a4d90ae0911ffd16b03"
+const apiKey = "c121be5508016a4d90ae0911ffd16b03";
 
 searchSubmit.addEventListener('click', e => {
   e.preventDefault();
+  if (searchInput.value.length === 0 || !searchInput.value.trim()) return;
   cityWeatherData();
 });
 searchInput.addEventListener('keyup', e => {
-  if(e.code === 'Enter') cityWeatherData();
+  if(e.code === 'Enter') {
+    if (searchInput.value.length === 0 || !searchInput.value.trim()) return;
+    cityWeatherData()
+  };
 });
 
 async function cityWeatherData() {
@@ -20,8 +24,9 @@ async function cityWeatherData() {
     displayWeatherData(weatherDatas[0], weatherDatas[1], name, country);
     searchInput.value = '';
   } catch(e) {
-    alert('Error 404 city not found!!')
-  }
+    alert('Error 404 city not found!!');
+    console.log(e);
+  };
 
 };
 
@@ -30,7 +35,7 @@ async function cityData() {
   const cityData = await fetch(getUrlCity(cityName));
   const cityDataJson = await cityData.json();
   return cityDataJson;
-}
+};
 
 async function weatherData(lat, lon) {
   const weatherData = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`);
@@ -39,7 +44,7 @@ async function weatherData(lat, lon) {
   const condition = weatherDataJson.weather[0].main;
   const weatherDatas = [temperature, condition]
   return weatherDatas;
-}
+};
 
 function displayWeatherData(temperature, condition, cityName, country) {
   let icon;
@@ -57,15 +62,15 @@ function displayWeatherData(temperature, condition, cityName, country) {
   }
   else if(condition === 'Mist') {
     icon = "assets/img/mist.svg";
-  }
+  };
   weatherDisplay.innerHTML = `
   <img src="${icon}" alt="weather-icon-status" class="weather-icon">
   <h2>${temperature}°c</h2>
   <h3>${cityName}, ${country}</h3>
   `;
-}
+};
 
  function getUrlCity(city) {
   const urlCity = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${apiKey}`;
   return urlCity;
- }
+ };
